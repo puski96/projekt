cf7e77df38371ec53ae26d17645938e56a9750fd
puski96 <72152563+puski96@users.noreply.github.com>
2021-05-12 22:26:28 +0300

    initial commit

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import img1 from "../img/isabel.jpg";
-import img2 from "../img/eggs10_chicken.jpg";
 
 function imageSlider() {
   return (
@@ -10,7 +8,7 @@ function imageSlider() {
       <hr />
       <Carousel interval={2000}>
         <Carousel.Item>
-          <img src={img1} height="600px" width="100%" alt="img1" />
+          <img src="../img/isabel.jpg" height="600px" width="100%" alt="img1" />
           <div className="in-left">
             <h1>Udvozollek</h1>
             <div>
@@ -20,7 +18,12 @@ function imageSlider() {
           </div>
         </Carousel.Item>
         <Carousel.Item>
-          <img src={img2} height="600px" width="100%" alt="img1" />
+          <img
+            src="../img/eggs10_chicken.jpg"
+            height="600px"
+            width="100%"
+            alt="img1"
+          />
         </Carousel.Item>
       </Carousel>
     </div>
